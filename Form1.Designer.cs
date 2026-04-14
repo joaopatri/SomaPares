bbc826d8a040a7a015f947fdb8ace4150fe693cf
Aluno <Aluno@MMA-LAB03-102>
2026-04-14 20:32:45 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SomaPares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.lblNum = new System.Windows.Forms.Label();
+			this.lblSoma = new System.Windows.Forms.Label();
+			this.txtNum = new System.Windows.Forms.TextBox();
+			this.txtSoma = new System.Windows.Forms.TextBox();
+			this.btnLimpar = new System.Windows.Forms.Button();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblNum
+			// 
+			this.lblNum.AutoSize = true;
+			this.lblNum.Location = new System.Drawing.Point(133, 82);
+			this.lblNum.Name = "lblNum";
+			this.lblNum.Size = new System.Drawing.Size(44, 13);
+			this.lblNum.TabIndex = 0;
+			this.lblNum.Text = "Numero";
+			// 
+			// lblSoma
+			// 
+			this.lblSoma.AutoSize = true;
+			this.lblSoma.Location = new System.Drawing.Point(143, 227);
+			this.lblSoma.Name = "lblSoma";
+			this.lblSoma.Size = new System.Drawing.Size(34, 13);
+			this.lblSoma.TabIndex = 1;
+			this.lblSoma.Text = "Soma";
+			// 
+			// txtNum
+			// 
+			this.txtNum.Location = new System.Drawing.Point(183, 75);
+			this.txtNum.Name = "txtNum";
+			this.txtNum.Size = new System.Drawing.Size(100, 20);
+			this.txtNum.TabIndex = 2;
+			// 
+			// txtSoma
+			// 
+			this.txtSoma.Location = new System.Drawing.Point(183, 220);
+			this.txtSoma.Name = "txtSoma";
+			this.txtSoma.Size = new System.Drawing.Size(100, 20);
+			this.txtSoma.TabIndex = 3;
+			// 
+			// btnLimpar
+			// 
+			this.btnLimpar.Location = new System.Drawing.Point(145, 137);
+			this.btnLimpar.Name = "btnLimpar";
+			this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+			this.btnLimpar.TabIndex = 4;
+			this.btnLimpar.Text = "Limpar";
+			this.btnLimpar.UseVisualStyleBackColor = true;
+			this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+			// 
+			// btnCalcular
+			// 
+			this.btnCalcular.Location = new System.Drawing.Point(241, 137);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+			this.btnCalcular.TabIndex = 5;
+			this.btnCalcular.Text = "Calcular";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.btnCalcular);
+			this.Controls.Add(this.btnLimpar);
+			this.Controls.Add(this.txtSoma);
+			this.Controls.Add(this.txtNum);
+			this.Controls.Add(this.lblSoma);
+			this.Controls.Add(this.lblNum);
+			this.Name = "Form1";
+			this.Text = "SomaPares";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblSoma;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtSoma;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomaPares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int i, num, soma = 0;
+            num = Convert.ToInt32(txtNum.Text);
+            for (i = 1; i <= num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    soma = soma + i;
+                }
+            }
+            txtSoma.Text = soma.ToString();
+		}
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtSoma.Clear();
+            txtNum.Clear();
+            txtNum.Focus();
+		}
+    }
+}
